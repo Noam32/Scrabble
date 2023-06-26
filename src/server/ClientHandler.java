25cdab99f579e5719eb6bad94f9ac84a246c7db7
Nadavzada <125977918+Nadavzada@users.noreply.github.com>
2023-06-26 19:08:05 +0300

    whole game code with mongoDB functions

---
+++ src/server/ClientHandler.java
@@ -0,0 +1,10 @@
+package server;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public interface ClientHandler {
+	void handleClient(InputStream inFromclient, OutputStream outToClient) throws Exception;
+	void close();
+}
